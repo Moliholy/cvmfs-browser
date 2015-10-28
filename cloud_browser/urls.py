775cbf4344dd022d72c1b278a9acc0d7f7c9f5bf
Jose Molina <jose.molina@cern.ch>
2015-10-28 14:51:45 +0100

    URL parsing updated

--- cloud_browser/urls.py
+++ cloud_browser/urls.py
@@ -7,10 +7,12 @@ from cloud_browser.app_settings import settings
 # pylint: disable=invalid-name
 urlpatterns = patterns(
     'cloud_browser.views',
-    url(r'^browser/(?P<repo_name>[a-z\.\-]+)$',
+    url(r'^browser/(?P<repo_name>[a-z\.\-]+)/$',
         'browser', name="cloud_browser_browser"),
     url(r'^browser/(?P<repo_name>[a-z\.\-]+)/(?P<revision>[^\/]*)/(?P<path>.*)$',
         'browser', name="cloud_browser_browser"),
+    url(r'^document/(?P<repo_name>[a-z\.\-]+)/$',
+        'document', name="cloud_browser_document"),
     url(r'^document/(?P<repo_name>[a-z\.\-]+)/(?P<revision>[^\/]*)/(?P<path>.*)$',
         'document', name="cloud_browser_document"),
 )
