0904da7b177deb27989862380a5b1c36a305c6a3
Jose Molina <jose.molina@cern.ch>
2015-10-28 14:52:55 +0100

    Removed unused code from config.py

--- cloud_browser/cloud/config.py
+++ cloud_browser/cloud/config.py
@@ -15,54 +15,11 @@ class Config(object):
 
         conn_cls = conn_fn = None
         datastore = settings.CLOUD_BROWSER_DATASTORE
-        if datastore == 'AWS':
-            # Try AWS
-            from cloud_browser.cloud.aws import AwsConnection
-            account = settings.CLOUD_BROWSER_AWS_ACCOUNT
-            secret_key = settings.CLOUD_BROWSER_AWS_SECRET_KEY
-            if account and secret_key:
-                conn_cls = AwsConnection
-                conn_fn = lambda: AwsConnection(account, secret_key)
-
-        if datastore == 'Google':
-            # Try Google Storage
-            from cloud_browser.cloud.google import GsConnection
-            account = settings.CLOUD_BROWSER_GS_ACCOUNT
-            secret_key = settings.CLOUD_BROWSER_GS_SECRET_KEY
-            if account and secret_key:
-                conn_cls = GsConnection
-                conn_fn = lambda: GsConnection(account, secret_key)
-
-        elif datastore == 'Rackspace':
-            # Try Rackspace
-            account = settings.CLOUD_BROWSER_RACKSPACE_ACCOUNT
-            secret_key = settings.CLOUD_BROWSER_RACKSPACE_SECRET_KEY
-            servicenet = settings.CLOUD_BROWSER_RACKSPACE_SERVICENET
-            authurl = settings.CLOUD_BROWSER_RACKSPACE_AUTHURL
-            if account and secret_key:
-                from cloud_browser.cloud.rackspace import RackspaceConnection
-                conn_cls = RackspaceConnection
-                conn_fn = lambda: RackspaceConnection(
-                    account,
-                    secret_key,
-                    servicenet=servicenet,
-                    authurl=authurl)
-
-        elif datastore == 'Filesystem':
-            # Mock filesystem
-            root = settings.CLOUD_BROWSER_FILESYSTEM_ROOT
-            if root is not None:
-                from cloud_browser.cloud.fs import FilesystemConnection
-                conn_cls = FilesystemConnection
-                conn_fn = lambda: FilesystemConnection(root)
-
-        elif datastore == 'CVMFilesystem':
+        if datastore == 'CVMFilesystem':
             # Use Cvmfs
             from cloud_browser.cloud.cernvmfs import CVMFilesystemConnection
-            cache_dir = settings.CLOUD_BROWSER_CVMFS_CACHE
             conn_cls = CVMFilesystemConnection
-            conn_fn = lambda: CVMFilesystemConnection(
-                settings.CLOUD_BROWSER_CVMFS_REPOSITORY_URL, cache_dir)
+            conn_fn = lambda: CVMFilesystemConnection()
 
         if conn_cls is None:
             raise ImproperlyConfigured(
@@ -89,7 +46,7 @@ class Config(object):
         return cls.__connection_cls
 
     @classmethod
-    def get_connection(cls):
+    def get_connection(cls, params=None):
         """Return connection object.
 
         :rtype: :class:`cloud_browser.cloud.base.CloudConnection`
@@ -98,4 +55,7 @@ class Config(object):
             if cls.__connection_fn is None:
                 _, cls.__connection_fn = cls.from_settings()
             cls.__connection_obj = cls.__connection_fn()
+        if params:
+            cls.__connection_obj.configure(params['url'],
+                                           params['revision'])
         return cls.__connection_obj
