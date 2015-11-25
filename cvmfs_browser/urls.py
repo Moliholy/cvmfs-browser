e48f56addb12d3de6d177ba63d122b674d56ddee
Jose Molina <jose.molina@cern.ch>
2015-11-25 10:02:01 +0100

    Fix URL handling

--- cvmfs_browser/urls.py
+++ cvmfs_browser/urls.py
@@ -7,12 +7,12 @@ from cvmfs_browser.app_settings import settings
 # pylint: disable=invalid-name
 urlpatterns = patterns(
     'cvmfs_browser.views',
-    url(r'^browser/(?P<repo_name>[a-z\.\-]+)/$',
+    url(r'^browser/(?P<repo_name>[a-z\.\-0-9]+)/$',
         'browser', name="cloud_browser_browser"),
-    url(r'^browser/(?P<repo_name>[a-z\.\-]+)/(?P<revision>[^\/]*)/(?P<path>.*)$',
+    url(r'^browser/(?P<repo_name>[a-z\.\-0-9]+)/(?P<revision>[^\/]*)/(?P<path>.*)$',
         'browser', name="cloud_browser_browser"),
-    url(r'^document/(?P<repo_name>[a-z\.\-]+)/$',
+    url(r'^document/(?P<repo_name>[a-z\.\-0-9]+)/$',
         'document', name="cloud_browser_document"),
-    url(r'^document/(?P<repo_name>[a-z\.\-]+)/(?P<revision>[^\/]*)/(?P<path>.*)$',
+    url(r'^document/(?P<repo_name>[a-z\.\-0-9]+)/(?P<revision>[^\/]*)/(?P<path>.*)$',
         'document', name="cloud_browser_document"),
 )
